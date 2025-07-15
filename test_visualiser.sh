cf1b7999e41c11c037d0a1a81b5bdaf010d3cb06
Simon Vermeersch <svermeer@f4r11s11.clusters.42paris.fr>
2025-07-15 03:52:00 +0200

    change COUNT variable name to SIZE

--- test_visualiser.sh
+++ test_visualiser.sh
@@ -3,10 +3,10 @@
 # === CONFIG ===
 PUSH_SWAP="./push_swap"           # Path to your push_swap binary
 VISUALIZER="visualizer_bonus/visualizer"         # Path to your visualizer binary
-COUNT=100                         # Number of random integers to generate
+SIZE=100                         # Number of random integers to generate
 
 # === STEP 1: Generate unique random numbers ===
-ARGS=$(seq 1 $COUNT | shuf | tr '\n' ' ')
+ARGS=$(seq 1 $SIZE | shuf | tr '\n' ' ')
 
 # === STEP 2: Run push_swap to get instructions ===
 OPS=$($PUSH_SWAP $ARGS)
